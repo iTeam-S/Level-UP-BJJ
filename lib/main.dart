c9c231d0ac0af4b7bb60b156e97213264fe698e4
Gaetan Jonathan <gaetan.s118@gmail.com>
2021-07-21 23:34:14 +0300

    Nouvel Structuration su projetb

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,31 @@
 import 'package:flutter/material.dart';
-import 'package:bjj_library/splash.dart';
-import 'package:bjj_library/login.dart';
 import 'package:get/get.dart';
-
+import 'package:bjj_library/view/splash.dart';
+import 'package:bjj_library/view/login.dart';
+import 'package:bjj_library/view/home.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-
-class MyApp extends StatelessWidget{
-	@override
-	Widget build(BuildContext context){
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
     Future.delayed(const Duration(milliseconds: 3000), () {
-      Get.to(LoginScreen());
+      Get.offNamed('/login');
     });
-		return GetMaterialApp(
-			theme: ThemeData(
-				primarySwatch: Colors.blue,
-				fontFamily: "ProductSans",
-			),
-			debugShowCheckedModeBanner: false,
-			home: SplashScreen(),
-		);
-	}
+    return GetMaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        fontFamily: "ProductSans",
+      ),
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/',
+      routes: {
+        '/': (context) => SplashScreen(),
+        '/login': (context) => LoginScreen(),
+        '/home': (context) => HomeScreen()
+      },
+    );
+  }
 }
