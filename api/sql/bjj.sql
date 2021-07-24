451660b2b3d5b520842afd0684eff75b233dc46f
Landris18 <landry.manankoraisina.rasendranirina@esti.mg>
2021-07-24 12:50:53 +0300

    Update bjj.sql

--- api/sql/bjj.sql
+++ api/sql/bjj.sql
@@ -28,7 +28,7 @@ CREATE TABLE `Module` (
   `date` date NOT NULL DEFAULT current_timestamp(),
   PRIMARY KEY (`id`),
   UNIQUE KEY `Module_UN` (`nom`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -37,7 +37,7 @@ CREATE TABLE `Module` (
 
 LOCK TABLES `Module` WRITE;
 /*!40000 ALTER TABLE `Module` DISABLE KEYS */;
-INSERT INTO `Module` VALUES (1,'Musculation','2021-07-20'),(2,'Course','2021-07-20'),(3,'Traction','2021-07-20'),(4,'Pompe','2021-07-20');
+INSERT INTO `Module` VALUES (1,'Coaching','2021-07-20'),(2,'Course','2021-07-20'),(3,'Traction','2021-07-20'),(4,'Pompe','2021-07-24');
 /*!40000 ALTER TABLE `Module` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -55,7 +55,7 @@ CREATE TABLE `Utilisateur` (
   `admin` tinyint(1) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `Utilisateur_UN` (`mail`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='Table pour les utilisateurs';
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='Table pour les utilisateurs';
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -64,8 +64,38 @@ CREATE TABLE `Utilisateur` (
 
 LOCK TABLES `Utilisateur` WRITE;
 /*!40000 ALTER TABLE `Utilisateur` DISABLE KEYS */;
-INSERT INTO `Utilisateur` VALUES (1,'landry.apsa@gmail.com','root18',1);
+INSERT INTO `Utilisateur` VALUES (1,'landry.apsa@gmail.com','root18',1),(2,'gaetan@gmail.com','root21',1),(3,'user@gmail.com','user21',0);
 /*!40000 ALTER TABLE `Utilisateur` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Video`
+--
+
+DROP TABLE IF EXISTS `Video`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Video` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `titre` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `date_upload` datetime NOT NULL DEFAULT current_timestamp(),
+  `nom` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `image` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `module_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `Video_FK` (`module_id`),
+  CONSTRAINT `Video_FK` FOREIGN KEY (`module_id`) REFERENCES `Module` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Video`
+--
+
+LOCK TABLES `Video` WRITE;
+/*!40000 ALTER TABLE `Video` DISABLE KEYS */;
+INSERT INTO `Video` VALUES (3,'Pompe 20 fois par jour, pourquoi ?','2021-07-24 11:47:50','1627116470.0758917johann_pachelbel_canon_in_d_-_YouTube.mkv','image_1627116470.0194767.jpg',1),(4,'Pompe 20 fois par jour, pourquoi ?','2021-07-24 12:06:13','1627117573.177341Hardwell__Maddix_-_Bella_Ciao_Live_Video.MP4','image_1627117573.1637814.jpg',1),(5,'Pompe 20 fois par jour, pourquoi ?','2021-07-24 12:07:37','1627117657.1987946Clean_Bandit_-_Symphony_feat._Zara_Larsson_Official_Video.MP4','image_1627117657.1874287.jpg',1),(6,'Pompe 20 fois par jour, pourquoi ?','2021-07-24 12:08:23','1627117703.3361063Dj_snake_Ocho_cinco.mp4','image_1627117703.3241723.jpg',1),(7,'Pompe 20 fois par jour, pourquoi ?','2021-07-24 12:12:11','1627117931.303617Chainsmokers__Coldplay_-_Something_Just_Like_This_Don_Diablo_Remix.MP4','image_1627117931.2916157.jpg',1),(8,'Pompe 20 fois par jour, pourquoi ?','2021-07-24 12:15:41','1627118141.6511772Hardwell__Maddix_-_Bella_Ciao_Live_Video.MP4','image_1627118141.6364164.jpg',1);
+/*!40000 ALTER TABLE `Video` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -77,4 +107,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-07-20 23:12:42
+-- Dump completed on 2021-07-24 12:49:48
