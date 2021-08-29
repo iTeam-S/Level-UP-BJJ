9ca09f7968e85a53044f4d4e636e7b26ea5f9be8
Landry Manankoraisina <47665507+Landris18@users.noreply.github.com>
2021-08-29 20:20:22 +0300

    Update conf.py

--- api/conf.py
+++
@@ -1,9 +0,0 @@
-from os import environ as env
-
-def database(**kwargs):
-     return {
-          'host' : 'localhost',
-          'user' : env.get('ITEAMS_DB_USER'),
-          'password': env.get('ITEAMS_DB_PASSWORD'),
-          'database': 'BJJ'
-     }
