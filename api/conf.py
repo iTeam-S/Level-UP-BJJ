f3af9a749d5b8122f5112cb1a40aba74f4fda76e
Gaetan Jonathan BAKARY <gaetan.jonathan.bakary@esti.mg>
2021-08-29 07:46:35 +0200

    Update conf.py

--- api/conf.py
+++ api/conf.py
@@ -3,7 +3,7 @@ from os import environ as env
 def database(**kwargs):
      return {
           'host' : 'localhost',
-          'user' : env.get('ITEAMS_DB_USER')
+          'user' : env.get('ITEAMS_DB_USER'),
           'password': env.get('ITEAMS_DB_PASSWORD'),
           'database': 'BJJ'
      }
